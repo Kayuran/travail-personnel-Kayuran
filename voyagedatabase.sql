8605b15c673da10699b70dda757c622179788b53
Kayuran <elv-kayuran.mhsln@eduge.ch>
2021-08-12 11:08:57 +0200

    Màj BDDO v1.0
    Mise à jour de la base de donnée en v1.0
    12.08.2021

--- voyagedatabase.sql
+++ voyagedatabase.sql
@@ -42,7 +42,7 @@ INSERT INTO `feedback` (`ranking`, `prestation_idPRE`, `users_idUser`) VALUES
 CREATE TABLE `prestation` (
   `idPRE` varchar(25) NOT NULL,
   `users_idUSER` int(25) NOT NULL,
-  `pays` varchar(100) DEFAULT NULL,
+  `pays` varchar(100) NOT NULL,
   `image` varchar(100) DEFAULT NULL,
   `description` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`idPRE`)
@@ -66,8 +66,7 @@ INSERT INTO `prestation` (`idPRE`,`users_idUSER`, `pays`,`image`, `description`)
 CREATE TABLE `hotel` (
   `idHOT` varchar(25) NOT NULL,
   `prestation_idPRE` varchar(25) NOT NULL,
-  `users_idUSER` int(25) NOT NULL,
-  `nom` varchar(100) DEFAULT NULL,
+  `nom` varchar(100) NOT NULL,
   `rating` float DEFAULT NULL,
   `image` varchar(100) DEFAULT NULL,
   `description` varchar(500) DEFAULT NULL,
@@ -79,19 +78,19 @@ CREATE TABLE `hotel` (
 -- Dumping data for table `hotel`
 --
 
-INSERT INTO `hotel` (`idHOT`, `prestation_idPRE`,`users_idUSER`,`nom`, `rating`,`image`, `description`) VALUES
-('0.1','1',1,'Riu Calypso', 4.5, '.\images\hotels\Canaries\Riu Calypso.jpg','Surplombant la plage de Jandía, cet hôtel contemporain est réservé aux adultes. Il se trouve à 2 km de la plage Playa del Matorral et à 4 km du parcours de golf de Jandía.'),
-('0.2','1',1,'Riu Palace Meloneras', 4.7, '.\images\hotels\Canaries\Riu Palace Meloneras.jpg','Offrant une vue sur les dunes de Maspalomas, sur la côte atlantique, ce complexe balnéaire élégant se trouve à 10 minutes à pied du phare de Maspalomas et à 6 km d Aqualand Maspalomas.'),
-('0.3','1',1,'Servatur Waikiki', 4.2, '.\images\hotels\Canaries\Servatur Waikiki.jpg','Cet hôtel tout compris à l atmosphère détendue se trouve à 12 minutes à pied de la Playa del Inglés, à 6 km du phare de Maspalomas et à 3 km des dunes de Maspalomas.'),
-('1.1','2',2,'Beach Albatros Resort', 4.5, '.\images\hotels\Egypte\Beach Albatros Resort.jpg','Situé au bord de la mer Rouge, ce vaste complexe tout compris se trouve à 2 km de l Hurghada Grand Aquarium et à 4 km du musée de sculptures Sand City Hurghada.'),
-('1.2','2',2,'Dana Beach Resort', 4.6, '.\images\hotels\Egypte\Dana Beach Resort.jpg','Situé au bord d une plage de la mer Rouge, ce vaste complexe haut de gamme se trouve à 5 km du Grand aquarium de Hurghada et à 14 km de l aéroport international de Hurghada.'),
-('1.3','2',2,'Steigenberger Aqua Magic', 4.5, '.\images\hotels\Egypte\Steigenberger Aqua Magic.jpg','Situé à 6 minutes à pied de la plage, ce complexe luxueux se trouve aussi à 6 km du Grand Aquarium et à 13 km du centre-ville de Hurghada.'),
-('2.1','3',3,'Hotel Pilot Beach Resort', 4.7, '.\images\hotels\Grece\Hotel Pilot Beach Resort.jpg','Situé dans une station balnéaire en bord de mer, ce complexe au style raffiné se trouve à 4,2 km du lac de Kournas et à 12 km du chai Dourakis Winery.'),
-('2.2','3',3,'Mitsis Grand Hotel Beach', 4.6, '.\images\hotels\Grece\Mitsis Grand Hotel Beach.jpg','Le Mitsis Grand Hotel Beach Hotel, un hôtel familial qui vous apporte le meilleur de Rhodes (ville) à votre portée.'),
-('2.3','3',3,'Neptune Hotels Resort', 4.8, '.\images\hotels\Grece\Neptune Hotels Resort.jpg','Situé sur la plage Bravo Beach, ce vaste complexe haut de gamme se trouve à 1,6 km du parc aquatique Lido et à 12 km du château d Antimachia.'),
-('3.1','4',3,'Kaya Belek', 4.5, '.\images\hotels\Turquie\Kaya Belek.jpg','Installé dans un jardin tropical au bord de la mer Méditerranée, ce complexe tout compris à l ambiance décontractée se trouve face à la plage, à 2 km du club de golf d Antalya et à 27 km de l aéroport d Antalya.'),
-('3.2','4',3,'Melas Lara Resort', 4.5, '.\images\hotels\Turquie\Melas Lara Resort.jpg','Installé sur la plage de Lara, cet élégant hôtel tout compris associe une décoration moderne avec des éléments traditionnels. Il se trouve à 12 km des chutes d eau panoramiques de Düden et à 9 km des sculptures de sable présentées à Sandland.'),
-('3.3','4',3,'Voyage Belek Golf Spa', 4.7, '.\images\hotels\Turquie\Voyage Belek Golf Spa.jpg','Situé au bord d une plage de sable de la mer Méditerranée, ce complexe haut de gamme se trouve à 37 km de l aéroport d Antalya et à 2 km de Belek Beach Park.');
+INSERT INTO `hotel` (`idHOT`, `prestation_idPRE`,`nom`, `rating`,`image`, `description`) VALUES
+('0.1','1','Riu Calypso', 4.5, '.\images\hotels\Canaries\Riu Calypso.jpg','Surplombant la plage de Jandía, cet hôtel contemporain est réservé aux adultes. Il se trouve à 2 km de la plage Playa del Matorral et à 4 km du parcours de golf de Jandía.'),
+('0.2','1','Riu Palace Meloneras', 4.7, '.\images\hotels\Canaries\Riu Palace Meloneras.jpg','Offrant une vue sur les dunes de Maspalomas, sur la côte atlantique, ce complexe balnéaire élégant se trouve à 10 minutes à pied du phare de Maspalomas et à 6 km d Aqualand Maspalomas.'),
+('0.3','1','Servatur Waikiki', 4.2, '.\images\hotels\Canaries\Servatur Waikiki.jpg','Cet hôtel tout compris à l atmosphère détendue se trouve à 12 minutes à pied de la Playa del Inglés, à 6 km du phare de Maspalomas et à 3 km des dunes de Maspalomas.'),
+('1.1','2','Beach Albatros Resort', 4.5, '.\images\hotels\Egypte\Beach Albatros Resort.jpg','Situé au bord de la mer Rouge, ce vaste complexe tout compris se trouve à 2 km de l Hurghada Grand Aquarium et à 4 km du musée de sculptures Sand City Hurghada.'),
+('1.2','2','Dana Beach Resort', 4.6, '.\images\hotels\Egypte\Dana Beach Resort.jpg','Situé au bord d une plage de la mer Rouge, ce vaste complexe haut de gamme se trouve à 5 km du Grand aquarium de Hurghada et à 14 km de l aéroport international de Hurghada.'),
+('1.3','2','Steigenberger Aqua Magic', 4.5, '.\images\hotels\Egypte\Steigenberger Aqua Magic.jpg','Situé à 6 minutes à pied de la plage, ce complexe luxueux se trouve aussi à 6 km du Grand Aquarium et à 13 km du centre-ville de Hurghada.'),
+('2.1','3','Hotel Pilot Beach Resort', 4.7, '.\images\hotels\Grece\Hotel Pilot Beach Resort.jpg','Situé dans une station balnéaire en bord de mer, ce complexe au style raffiné se trouve à 4,2 km du lac de Kournas et à 12 km du chai Dourakis Winery.'),
+('2.2','3','Mitsis Grand Hotel Beach', 4.6, '.\images\hotels\Grece\Mitsis Grand Hotel Beach.jpg','Le Mitsis Grand Hotel Beach Hotel, un hôtel familial qui vous apporte le meilleur de Rhodes (ville) à votre portée.'),
+('2.3','3','Neptune Hotels Resort', 4.8, '.\images\hotels\Grece\Neptune Hotels Resort.jpg','Situé sur la plage Bravo Beach, ce vaste complexe haut de gamme se trouve à 1,6 km du parc aquatique Lido et à 12 km du château d Antimachia.'),
+('3.1','4','Kaya Belek', 4.5, '.\images\hotels\Turquie\Kaya Belek.jpg','Installé dans un jardin tropical au bord de la mer Méditerranée, ce complexe tout compris à l ambiance décontractée se trouve face à la plage, à 2 km du club de golf d Antalya et à 27 km de l aéroport d Antalya.'),
+('3.2','4','Melas Lara Resort', 4.5, '.\images\hotels\Turquie\Melas Lara Resort.jpg','Installé sur la plage de Lara, cet élégant hôtel tout compris associe une décoration moderne avec des éléments traditionnels. Il se trouve à 12 km des chutes d eau panoramiques de Düden et à 9 km des sculptures de sable présentées à Sandland.'),
+('3.3','4','Voyage Belek Golf Spa', 4.7, '.\images\hotels\Turquie\Voyage Belek Golf Spa.jpg','Situé au bord d une plage de sable de la mer Méditerranée, ce complexe haut de gamme se trouve à 37 km de l aéroport d Antalya et à 2 km de Belek Beach Park.');
 
 -- --------------------------------------------------------
 
@@ -121,29 +120,6 @@ INSERT INTO `users` (`idUSER`, `eMail`, `password`,`nom`, `prenom`) VALUES
 (4, 'jesse@gmail.com', 'dc76e9f0c0006e8f919e0c515c66dbba3982f785','Jessie','James');
 
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reservation`
---
-
-CREATE TABLE `reservation` (
-  `nb_personne` varchar(100) NOT NULL,
-  `users_idUSER`int(25) NOT NULL,
-  `hotel_idHOT`varchar(25) NOT NULL,
-  PRIMARY KEY (`users_idUSER`,`hotel_idHOT`),
-  KEY `FK_reservation_idHOT_idx` (`hotel_idHOT`),
-  KEY `FK_reservation_idUser_idx` (`users_idUSER`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
---
--- Dumping data for table `reservation`
---
-
-INSERT INTO `reservation` (`hotel_idHOT`, `users_idUSER`, `nb_personne`) VALUES
-('1', 2, '2');
 
 
 -- --------------------------------------------------------
@@ -186,11 +162,8 @@ ALTER TABLE `feedback`
   ADD CONSTRAINT `FK_feedback_idUser` FOREIGN KEY (`users_idUser`) REFERENCES `users` (`idUser`);
 
 ALTER TABLE `hotel`
-  ADD CONSTRAINT `FK_hotel_idPRE` FOREIGN KEY (`prestation_idPRE`) REFERENCES `prestation` (`idPRE`),
-  ADD CONSTRAINT `FK_hotel_idUser` FOREIGN KEY (`users_idUser`) REFERENCES `users` (`idUser`);
+  ADD CONSTRAINT `FK_hotel_idPRE` FOREIGN KEY (`prestation_idPRE`) REFERENCES `prestation` (`idPRE`);
 
-ALTER TABLE `reservation`
-  ADD CONSTRAINT `FK_reservation_idHOT` FOREIGN KEY (`hotel_idHOT`) REFERENCES `hotel` (`idHOT`);
 
 ALTER TABLE `prestation`
   ADD CONSTRAINT `FK_prestation_idUser` FOREIGN KEY (`users_idUser`) REFERENCES `users` (`idUser`);
